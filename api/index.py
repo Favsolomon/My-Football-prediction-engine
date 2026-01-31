69db1928ef53c7a3f0d24d9a1626a31a7942eedf
favsolomon <favagoziesolomon@gmail.com>
2026-01-31 10:29:19 +0100

    bug fix

--- api/index.py
+++ api/index.py
@@ -1,1 +1,7 @@
-from .main import app
+import sys
+import os
+
+# Add the project root (parent of api/) to sys.path so 'src' can be imported
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from api.main import app
